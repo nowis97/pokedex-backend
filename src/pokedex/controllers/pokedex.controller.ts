a83093bc0227a60c399e5835176e0da514915efd
Simón Espinosa <saespinosac@gmail.com>
2025-06-01 18:46:15 -0400

    fix: query all pokemons

--- src/pokedex/controllers/pokedex.controller.ts
+++ src/pokedex/controllers/pokedex.controller.ts
@@ -3,20 +3,20 @@ import { PokedexService } from '../services/pokedex.service';
 
 @Controller('pokemon')
 export class PokedexController {
-    constructor(private readonly pokedexService: PokedexService) { }
+  constructor(private readonly pokedexService: PokedexService) {}
 
-    @Get()
-    getPokemons(@Query('offset') offset: number, @Query('limit') limit: number) {
-        return this.pokedexService.getPokemons(offset, limit);
-    }
+  @Get()
+  getPokemons(@Query('offset') offset: number, @Query('limit') limit: number) {
+    return this.pokedexService.getPokemons(offset, limit);
+  }
 
-    @Get(':id_or_name')
-    getPokemon(@Param('id_or_name') idOrName: string) {
-        return this.pokedexService.getPokemon(idOrName);
-    }
+  @Get('search')
+  searchPokemon(@Query('q') query: string) {
+    return this.pokedexService.searchPokemon(query);
+  }
 
-    @Get('search')
-    searchPokemon(@Query('q') q: string) {
-        return this.pokedexService.searchPokemon(q);
-    }
+  @Get(':id_or_name')
+  getPokemon(@Param('id_or_name') idOrName: string) {
+    return this.pokedexService.getPokemon(idOrName);
+  }
 }
